814ca6869398e1d7ebbffa8f154b1144a503cc4f
mpadge <mark.padgham@email.com>
2017-09-29 19:57:16 +0200

    fix typo in streetnet fn

--- R/streetnet.R
+++ R/streetnet.R
@@ -23,7 +23,7 @@ get_streetnet <- function (city)
             osmdata::add_osm_feature (key = "highway") %>%
             osmdata::osmdata_sf (quiet = FALSE)
 
-    dodgr::weight_streenet (dat$osmlines, profile = "bicycle")
+    dodgr::weight_streetnet (dat$osmlines, wt_profile = "bicycle")
 }
 
 #' get_all_networks
